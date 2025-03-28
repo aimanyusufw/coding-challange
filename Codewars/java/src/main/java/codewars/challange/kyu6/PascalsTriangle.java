d1f7bcf0feb716598f684f2658f603686d8f3cdd
aimanyusufw <aimanyusufdev@gmail.com>
2025-03-28 14:25:11 +0700

    ref : error git

--- Codewars/java/src/main/java/codewars/challange/kyu6/PascalsTriangle.java
+++ Codewars/java/src/main/java/codewars/challange/kyu6/PascalsTriangle.java
@@ -31,13 +31,13 @@ public class PascalsTriangle {
     // Method 2
 
     // Other User Method
-    static long[] generate(int level) {
-        long[] pascal = new long[level * (level + 1) / 2];
-        for (int i = 0, l = 0; l < level; l++) {
-        for (long f = 1, k = 0; k <= l; f = f * (l - k) / (k + 1), k++) {
-            pascal[i++] = f;
-        }
-        }
-        return pascal;
-    }
+    // static long[] generate(int level) {
+    //     long[] pascal = new long[level * (level + 1) / 2];
+    //     for (int i = 0, l = 0; l < level; l++) {
+    //     for (long f = 1, k = 0; k <= l; f = f * (l - k) / (k + 1), k++) {
+    //         pascal[i++] = f;
+    //     }
+    //     }
+    //     return pascal;
+    // }
 }
