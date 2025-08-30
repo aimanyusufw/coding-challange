035127a657e15c6f556552ad77873fac3cdf1436
aimanyusufw <aimanyusufdev@gmail.com>
2025-08-30 15:40:43 +0700

    finish : <dart> codewars 8 level 8kyu Is the string uppercase

---
+++ Codewars/dart/bin/8kyu/8-isUpperCase.dart
@@ -0,0 +1,16 @@
+// Codewars 8
+// Title :  Is the string uppercase
+// Instructions :   Create a method to see whether the string is ALL CAPS.
+// Challange Url :  https://www.codewars.com/kata/56cd44e1aa4ac7879200010b/train/dart
+// Level : 8kyu
+
+// Method : 1
+// bool isUpperCase(String s) {
+//   return s == s.toUpperCase();
+// }
+
+// Method 2
+bool isUpperCase(String s) => s == s.toUpperCase();
+
+// Other Users Method
+// bool isUpperCase(String str) => str.toUpperCase() == str;
---
+++ Codewars/dart/test/8kyu/8-isUpperCase_test.dart
@@ -0,0 +1,26 @@
+import "package:test/test.dart";
+
+import "../../bin/8kyu/8-isUpperCase.dart";
+
+void main() {
+  group("Fixed tests", () {
+    test("Testing for c", () => expect(isUpperCase('c'), equals(false)));
+    test("Testing for C", () => expect(isUpperCase('C'), equals(true)));
+    test(
+      "Testing for hello I AM DONALD",
+      () => expect(isUpperCase('hello I AM DONALD'), equals(false)),
+    );
+    test(
+      "Testing for HELLO I AM DONALD",
+      () => expect(isUpperCase('HELLO I AM DONALD'), equals(true)),
+    );
+    test(
+      "Testing for ACSKLDFJSgSKLDFJSKLDFJ",
+      () => expect(isUpperCase('ACSKLDFJSgSKLDFJSKLDFJ'), equals(false)),
+    );
+    test(
+      "Testing for ACSKLDFJSGSKLDFJSKLDFJ",
+      () => expect(isUpperCase('ACSKLDFJSGSKLDFJSKLDFJ'), equals(true)),
+    );
+  });
+}
