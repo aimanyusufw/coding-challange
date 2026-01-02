7bd1db9b7152cdf008f0266a18f2624a18218c87
aimanyusufw <aimanyusufdev@gmail.com>
2026-01-02 20:22:14 +0700

    finish : <dart> codewars 15 level 8kyu Get Nth Even Number

---
+++ Codewars/dart/bin/8kyu/15-nthEven.dart
@@ -0,0 +1,16 @@
+// Codewars 15
+// Title : Get Nth Even Number
+// Instructions :   Return the Nth Even Number
+// Challange Url :  https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/dart
+// Level : 8kyu
+
+// Method : 1
+// int nthEven(int n) {
+//   return 2 * (n - 1);
+// }
+
+// Method : 2
+int nthEven(int n) => 2 * (n - 1);
+
+// Other Users Method
+// int nthEven(int n) => 2 * (n - 1);
---
+++ Codewars/dart/test/8kyu/15-nthEven_test.dart
@@ -0,0 +1,15 @@
+import "package:test/test.dart";
+import "../../bin/8kyu/15-nthEven.dart";
+
+void main() {
+  group('Fixed tests', () {
+    test("Testing for 1", () => expect(nthEven(1), equals(0)));
+    test("Testing for 2", () => expect(nthEven(2), equals(2)));
+    test("Testing for 3", () => expect(nthEven(3), equals(4)));
+    test("Testing for 100", () => expect(nthEven(100), equals(198)));
+    test(
+      "Testing for 1298734",
+      () => expect(nthEven(1298734), equals(2597466)),
+    );
+  });
+}
