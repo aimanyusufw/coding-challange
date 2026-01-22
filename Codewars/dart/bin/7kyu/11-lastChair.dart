0e7181da2e31f0d9981f94cd87d75a241153d1d6
aimanyusufw <aimanyusufdev@gmail.com>
2026-01-22 20:53:30 +0700

    finish : <dart> codewars 10 level 7kyu

---
+++ Codewars/dart/bin/7kyu/11-lastChair.dart
@@ -0,0 +1,16 @@
+// Codewars 11
+// Title :  Waiting room
+// Instructions : Your task is to find last patient's chair's number.
+// Challange Url :  https://www.codewars.com/kata/542f0c36d002f8cd8a0005e5/dart
+// Level : 7 kyu
+
+// Method : 1
+// int lastChair(int n) {
+//   return n - 1;
+// }
+
+// Method 2
+int lastChair(int n) => n - 1;
+
+// Other Users Method
+// int lastChair(int n) => n - 1;
---
+++ Codewars/dart/test/7kyu/11-lastChair_test.dart
@@ -0,0 +1,8 @@
+import "package:test/test.dart";
+import "../../bin/7kyu/11-lastChair.dart";
+
+void main() {
+  group("sample test", () {
+    test("Testing for 10", () => expect(lastChair(10), equals(9)));
+  });
+}
